beccb1b0db6ec8754a413260a41aa828a2168bd9
s22001 <s22001@std.it-college.ac.jp>
2023-05-31 12:07:37 +0900

    Spring Boot initial commit

---
+++ src/test/kotlin/jp/ac/it_college/std/s22001/demo/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s22001.demo.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
